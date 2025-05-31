31f0679554fad0a73ec2c43a46d10a6a655073ab
Miguel Amorim <mferradaes@gmail.com>
2025-05-30 21:18:52 -0300

    Integraçao da api com outlet
     integraçao da api com outlet e filtragem da api

--- api/api.js
+++ api/api.js
@@ -9,14 +9,46 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+  // Palavras que geralmente indicam tênis
+const isSneaker = (product) => {
+  const name = product.shoeName?.toLowerCase() || '';
+
+  // Palavras que indicam roupas ou acessórios
+  
+  const clothingKeywords = [
+     // Roupas
+    'jersey', 'shirt', 'tee', 't-shirt', 'long sleeve', 'crewneck',
+    'hoodie', 'jacket', 'pants', 'shorts', 'socks', 'sweatshirt',
+    'fear of god',
+
+    // Acessórios
+    'hat', 'cap', 'beanie', 'glasses', 'sunglasses', 'ring', 'bracelet', 'chain',
+
+    // Bolsas e afins
+    'bag', 'backpack', 'pouch', 'tote', 'duffle', 'bandouliere',
+
+    // Itens de viagem / carteira
+    'wallet', 'passport', 'cover', 'cardholder', 'card holder'
+
+  ];
+  
+
+
+  return !clothingKeywords.some(keyword => name.includes(keyword));
+};
+
+
+
 // Buscar produtos por nome (ex: Yeezy, Jordan)
 app.get('/search', (req, res) => {
   const query = req.query.q;
   if (!query) return res.status(400).json({ error: 'Parâmetro "q" é obrigatório' });
 
-  sneaks.getProducts(query, 10, (err, products) => {
+  sneaks.getProducts(query, 600, (err, products) => {
     if (err) return res.status(500).json({ error: 'Erro ao buscar produtos' });
-    res.json(products);
+
+    const filtered = products.filter(isSneaker);
+    res.json(filtered);
   });
 });
 
@@ -31,9 +63,11 @@ app.get('/product/:styleID', (req, res) => {
 
 // Produtos mais populares
 app.get('/popular', (req, res) => {
-  sneaks.getMostPopular(10, (err, products) => {
+  sneaks.getMostPopular(100   , (err, products) => {
     if (err) return res.status(500).json({ error: 'Erro ao buscar produtos populares' });
-    res.json(products);
+
+    const filtered = products.filter(isSneaker);
+    res.json(filtered);
   });
 });
 
