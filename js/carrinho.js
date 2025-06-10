65139f9177afd99e2ac512153fcec344bb26d7d2
Guikino <macedoguilherme55@gmail.com>
2025-06-10 00:41:54 -0300

    alguma parada ai

---
+++ js/carrinho.js
@@ -0,0 +1,16 @@
+import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm";
+
+
+const userId = localStorage.getItem("userId")
+
+async function dataUser(){
+    let avatarHeader = document.querySelector("#avatar")  
+    const loginLink = document.querySelector("#login-link")
+    if(userId){
+        const dataUserAvatar = await axios.get(`http://localhost:8081/users/${userId}`)
+        console.log(dataUserAvatar.data.id)
+        avatarHeader.src = dataUserAvatar.data.avatar
+        loginLink.href = "../pages/conta.html"
+    }
+  }
+dataUser()
